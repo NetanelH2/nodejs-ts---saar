328260cc2537c080f5febca33c1a16fb9ea65a4d
Netanel Harush <netanelharush2@gmail.com>
2025-01-07 17:20:23 +0200

    Update .gitignore, add dotenv dependency, and configure MongoDB connection using environment variables

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const connectToDB = async () => {
   try {
-    await mongoose.connect(
-      'mongodb+srv://netanelh2-user:YfbcS6Vkc3ZtunUM@cluster0.wpyxv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    )
+    await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING)
     console.log('Connected to MongoDB Successfully')
   } catch (error) {
     console.error(`Could not connect to MongoDB with error:\n${error}`)
